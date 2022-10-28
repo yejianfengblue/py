50c78995823cbb91eadb5495256f1b3092f63bf2
Blue Ye <yejianfengblue@163.com>
2022-10-28 17:19:18 +0800

    Add appium script to cancel Wechat subscriptions

---
+++ wechat_cancel_subscriptions.py
@@ -0,0 +1,22 @@
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.mobileby import MobileBy
+
+options = UiAutomator2Options()
+options.platformVersion = '10'
+options.udid='62f472779807'
+options.no_reset=True
+options.adb_exec_timeout=60
+options.android_install_timeout=600
+options.new_command_timeout=600
+
+driver = webdriver.Remote('http://127.0.0.1:4723', options=options)
+
+while True:
+    account = driver.find_element(AppiumBy.ID, value='com.tencent.mm:id/aip')
+    TouchAction(driver).long_press(account).perform()
+    sleep(1)
+    driver.find_element(MobileBy.ANDROID_UIAUTOMATOR, value='new UiSelector().textMatches("不再关注")').click()
+    sleep(1)
+    driver.find_element(MobileBy.ANDROID_UIAUTOMATOR, value='new UiSelector().textMatches("不再关注")').click()
+    sleep(1)
